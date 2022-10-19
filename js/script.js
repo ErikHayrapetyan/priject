371bcb87da77e3c2cfed8373d85c43eaacda4b2c
vazgen <exap@mail.com>
2022-10-19 13:07:44 +0400

    frist version

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let numeber = 12;
+
+let vazgen = "davay brat";
+
+const ler = 10;
+
+const dar = false;
+
+const artu = {
+    age: 13,
+    hiscolor: "blue",
+    ishestupid: true,
+
+
+};
+
+const ansver = +prompt("imya tvayo");
+console.log(typeof (ansver));
+console.log(ansver);
